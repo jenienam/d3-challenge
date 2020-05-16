939392ac78c14c51deb45448c4bb2a905e0243f3
Jenie Nam <jenien@uci.edu>
2020-05-15 23:03:51 -0700

    Updated hw

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -26,22 +26,21 @@ var svg = d3.select("#scatter")
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 // Import Data
-d3.csv("assets/data/data.csv").then(function(analyze) {
+d3.csv("assets/data/data.csv").then(function(analyze_data) {
       // Step 1: Parse Data/Cast as numbers
     // ==============================
-    analyze.forEach(function(data) {
+    analyze_data.forEach(function(data) {
         data.poverty = +data.poverty;
         data.obesity = +data.obesity;
       });
-    });
 //scale
-    let xLinearScale = d3.scaleLinsear()
-    .domain([20, d3.max(analyze, d => d.poverty)])
-    .range([0, width]);
+    let xLinearScale = d3.scaleLinear()
+        .domain([20, d3.max(analyze_data, data => data.poverty)])
+        .range([0, plot_width]);
 
     let yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(analyze, d => d.obesity)])
-    .range([height, 0]);
+        .domain([0, d3.max(analyze_data, data => data.obesity)])
+        .range([plot_height, 0]);
 
   // Step 3: Create axis functions
     // ==============================
@@ -51,7 +50,7 @@ d3.csv("assets/data/data.csv").then(function(analyze) {
     // Step 4: Append Axes to the chart
     // ==============================
     chartGroup.append("g")
-      .attr("transform", `translate(0, ${height})`)
+      .attr("transform", `translate(0, ${plot_height})`)
       .call(bottomAxis);
 
     chartGroup.append("g")
@@ -60,23 +59,23 @@ d3.csv("assets/data/data.csv").then(function(analyze) {
     // Step 5: Create Circles
     // ==============================
     let circlesGroup = chartGroup.selectAll("circle")
-    .data(analyze)
-    .enter()
-    .append("circle")
-    .attr("cx", d => xLinearScale(d.poverty))
-    .attr("cy", d => yLinearScale(d.obesity))
-    .attr("r", "15")
+        .data(analyze_data)
+        .enter()
+        .append("circle")
+        .attr("cx", data => xLinearScale(data.poverty))
+        .attr("cy", data => yLinearScale(data.obesity))
+        .attr("r", "15")
 
      // Step 6: Create text label for circles
     // ===============================
     let text_labels = chartGroup.selectAll("circle")
-    .data(analyze)
-    .enter()
-    .append("text")
-    .text(data => data.abbr)
-    .attr("class", "stateText")
-    .attr("dx", data => xLinearScale(data.poverty))
-    .attr("dy", data => yLinearScale(data.obesity))
+        .data(analyze_data)
+        .enter()
+        .append("text")
+        .text(data => data.abbr)
+        .attr("class", "stateText")
+        .attr("dx", data => xLinearScale(data.poverty))
+        .attr("dy", data => yLinearScale(data.obesity))
     
 
     // Step 6: Initialize tool tip
@@ -86,7 +85,7 @@ d3.csv("assets/data/data.csv").then(function(analyze) {
       .offset([80, -60])
       .html(function(data) {
         return (`${data.state}<br>Poverty: ${data.poverty}<br>Obesity: ${data.obesity}`);
-      });
+    });
 
     // Step 7: Create tooltip in the chart
     // ==============================
@@ -106,12 +105,13 @@ d3.csv("assets/data/data.csv").then(function(analyze) {
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 0 - margin.left + 40)
-      .attr("x", 0 - (height / 2))
+      .attr("x", 0 - (plot_height / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
       .text("Percentage of Obesity: ");
 
     chartGroup.append("text")
-      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("transform", `translate(${plot_width / 2}, ${plot_height + margin.top + 30})`)
       .attr("class", "axisText")
       .text("Percentage of Poverty");
+});
